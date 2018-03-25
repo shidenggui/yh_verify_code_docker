2de142e8ab4df1fed565a5fa567d2c5f3891ab2b
shidenggui <903618848@qq.com>
2018-03-25 22:32:38 +0800

    feat: add influxdb monitor

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # coding:utf8
 import re
+import os
+import time
+import datetime
 from argparse import ArgumentParser
 
 import pytesseract
@@ -14,16 +17,46 @@ def yh():
     image = request.files['image']
     image = Image.open(image)
     image = remove_noise(image)
-    code = pytesseract.image_to_string(image, lang='chi_sim', config='-psm 7 digits')
+    code = pytesseract.image_to_string(
+        image, lang='chi_sim', config='-psm 7 digits')
     return map_numbers(code)
 
 
 @app.route('/yh_client', methods=['POST'])
 def yh_client():
+    start = time.time()
     image = request.files['image']
     image = Image.open(image)
     code = pytesseract.image_to_string(image, config='-psm 7')
+    use_time = time.time() - start()
+    try:
+        write_to_influxdb(use_time)
+    except:
+        pass
     return jsonify({'result': code}), 201
+
+
+def write_to_influxdb(use_time):
+    if os.getenv('INFLUXDB_MONITOR') is None:
+        return
+    from influxdb import InfluxDBClient
+    client = InfluxDBClient(
+        os.getenv('INFLUXDB_HOST'),
+        port=os.getenv('INFLUXDB_PORT'),
+        username=os.getenv('INFLUXDB_USER'),
+        password=os.getenv('INFLUXDB_PASSWORD'),
+        database=os.getenv('INFLUXDB_DB'))
+    json_body = [{
+        "measurement": "yh_verify",
+        "tags": {
+            "host": request.remote_addr,
+        },
+        "time": str(datetime.datetime.now()),
+        "fields": {
+            "use_time": use_time
+        }
+    }]
+    client.write_points(json_body)
 
 
 def remove_noise(image):
@@ -36,11 +69,30 @@ def remove_noise(image):
 
 
 def map_numbers(code):
-    valid_chars = re.findall('[〇一二三四五六七八九零壹贰叁肆伍陆柒捌玖]'.decode('utf8'), code.decode('utf8'), re.IGNORECASE)
-    label_map = {u'〇': u'0', u'一': u'1', u'二': u'2', u'三': u'3', u'四': u'4', u'五': u'5', u'六': u'6', u'七': u'7',
-                 u'八': u'8', u'九': u'9',
-                 u'零': u'0', u'壹': u'1', u'贰': u'2', u'叁': u'3', u'肆': u'4', u'伍': u'5', u'陆': u'6', u'柒': u'7',
-                 u'捌': u'8', u'玖': u'9'}
+    valid_chars = re.findall('[〇一二三四五六七八九零壹贰叁肆伍陆柒捌玖]'.decode('utf8'),
+                             code.decode('utf8'), re.IGNORECASE)
+    label_map = {
+        u'〇': u'0',
+        u'一': u'1',
+        u'二': u'2',
+        u'三': u'3',
+        u'四': u'4',
+        u'五': u'5',
+        u'六': u'6',
+        u'七': u'7',
+        u'八': u'8',
+        u'九': u'9',
+        u'零': u'0',
+        u'壹': u'1',
+        u'贰': u'2',
+        u'叁': u'3',
+        u'肆': u'4',
+        u'伍': u'5',
+        u'陆': u'6',
+        u'柒': u'7',
+        u'捌': u'8',
+        u'玖': u'9'
+    }
     v_c = []
     for e in valid_chars:
         v_c.append(label_map[e])
